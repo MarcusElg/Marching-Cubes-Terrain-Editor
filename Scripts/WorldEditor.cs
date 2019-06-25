51361d6597173852d89eda5bac14c55b8fe35cf4
MCrafterzz <marcusaccounts@yahoo.se>
2019-06-25 10:48:46 +0200

    Fixed set brush inspector showing force over distance variable

--- Scripts/WorldEditor.cs
+++ Scripts/WorldEditor.cs
@@ -143,7 +143,6 @@ public class WorldEditor : Editor
     {
         EditorGUI.BeginChangeCheck();
         serializedObject.FindProperty("range").intValue = Mathf.Clamp(EditorGUILayout.IntField("Range", serializedObject.FindProperty("range").intValue), 1, Mathf.RoundToInt(serializedObject.FindProperty("chunkSize").intValue * 0.375f * world.transform.lossyScale.x));
-        serializedObject.FindProperty("forceOverDistance").animationCurveValue = EditorGUILayout.CurveField("Force Over Distance", serializedObject.FindProperty("forceOverDistance").animationCurveValue);
         serializedObject.FindProperty("targetHeight").intValue = Mathf.Clamp(EditorGUILayout.IntField("Target Height", serializedObject.FindProperty("targetHeight").intValue), 1, serializedObject.FindProperty("chunkSize").intValue * serializedObject.FindProperty("maxHeightIndex").intValue);
 
         if (GUILayout.Button("Set All") == true)
